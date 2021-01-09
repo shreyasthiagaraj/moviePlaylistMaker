86343030ec0a379ce9d83fe7ee2a56d39bfda3eb
Shreyas Thiagaraj <shreyas.thiagaraj@gmail.com>
2021-01-08 20:44:50 -0500

    Initial Commit

---
+++ MoviePlaylist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviePlaylist
+//
+//  Created by Shreyas Thiagaraj on 1/8/21.
+//  Copyright © 2021 Shreyas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
